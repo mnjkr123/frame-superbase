9a7624f54ed6e16c5435fecf1b39ac5b692db4a6
mnjkr123 <ekmtutor@gmail.com>
2024-05-30 18:41:37 -0700

    Changed Layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ export const viewport = {
   initialScale: 1.0,
 };
 
+export const metadata = {
+  metadataBase: new URL('https://frame-superbase-cepn6agte-manoj-kumars-projects-972aa0cf.vercel.app'),
+  // Other metadata properties
+  title: 'SuperBase',
+  description: 'SuperBase',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
