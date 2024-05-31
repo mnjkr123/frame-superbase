722acbad70258781d8c08e65618fc4980521bc5f
mnjkr123 <ekmtutor@gmail.com>
2024-05-30 21:01:15 -0700

    other changes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,8 @@
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
 };
+
+export default config;
